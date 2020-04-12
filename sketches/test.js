56d2dc2951c393822944b6be7b2b2bc4713392db
juanfer <jramirezmon@unal.edu.co>
2020-04-12 15:20:23 -0500

    Se agregaron carpetas restantes

--- sketches/test.js
+++ sketches/test.js
@@ -1,22 +1,3 @@
-//var myCanvas = createCanvas(400, 400);
-    //myCanvas.parent('for_id');
-//<img src="Images/Example5.png"/>
-
-//var x = 100;
-//var y = 100;
-
-//function setup() {
-//  createCanvas(400, 400);
-//  var myCanvas = createCanvas(400, 400);
-//}
-
-//function draw() {
-//  background(0);
-//  fill(255);
-//  ellipse(30,20,50,50);
-//}
-//__________________________________________________________________________________
-
 //Triángulo Grande 1:
 var  angleT = 3.1415/4;
 var  posxT = 100;
@@ -104,24 +85,3 @@ function draw(){
   pintarTriangulo();
   pintarParalelogramo();
 }
-//var sketch1 = function( p ) {
-//    p.setup = function() {
-//        p.createCanvas(400, 400);
-//    };
-//};
-//var myp5_1 = new p5(sketch1, 'mysketch_id');
-//var myp5 = new p5( function( sketch ) {
-//
-//    var x = 100; 
-//    var y = 100;
-//  
-//    sketch.setup = function() {
-//      sketch.createCanvas(200, 200);
-//    };
-//  
-//    sketch.draw = function() {
-//      sketch.background(0);
-//      sketch.fill(255);
-//      sketch.rect(x,y,50,50);
-//    };
-//  });
